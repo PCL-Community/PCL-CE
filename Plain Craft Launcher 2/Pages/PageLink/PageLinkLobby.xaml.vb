b48e031d2e89c87ea34f96727ccfb6b5e284d3c0
Pigeon0v0 <60414767+Pigeon0v0@users.noreply.github.com>
2025-07-05 12:50:36 +0800

    fix: 意外的登录过期提示 ui: 添加一些条款

--- Plain Craft Launcher 2/Pages/PageLink/PageLinkLobby.xaml.vb
+++ Plain Craft Launcher 2/Pages/PageLink/PageLinkLobby.xaml.vb
@@ -20,12 +20,14 @@
 
     Public IsLoad As Boolean = False
     Public Sub Reload() Handles Me.Loaded
+        If IsLoad Then Exit Sub
+        IsLoad = True
         HintAnnounce.Visibility = Visibility.Visible
         HintAnnounce.Text = "正在连接到大厅服务器..."
         HintAnnounce.Theme = MyHint.Themes.Blue
         RunInNewThread(Sub()
                            If Not Setup.Get("LinkEula") Then
-                               Select Case MyMsgBox($"在使用 PCL CE 大厅之前，请阅读并同意以下条款：{vbCrLf}{vbCrLf}我承诺严格遵守中国大陆相关法律法规，不会将大厅功能用于违法违规用途。{vbCrLf}我承诺使用大厅功能带来的一切风险自行承担。{vbCrLf}我已知晓并同意 PCL CE 收集经处理的本机识别码、Natayark ID 与其他信息并在必要时提供给执法部门。{vbCrLf}{vbCrLf}另外，你还需要同意 PCL CE 大厅相关隐私政策及《Natayark OpenID 服务条款》。", "联机大厅协议授权",
+                               Select Case MyMsgBox($"在使用 PCL CE 大厅之前，请阅读并同意以下条款：{vbCrLf}{vbCrLf}我承诺严格遵守中国大陆相关法律法规，不会将大厅功能用于违法违规用途。{vbCrLf}我承诺使用大厅功能带来的一切风险自行承担。{vbCrLf}我已知晓并同意 PCL CE 收集经处理的本机识别码、Natayark ID 与其他信息并在必要时提供给执法部门。{vbCrLf}为保护未成年人个人信息，使用联机大厅前，我确认我已满十四周岁。{vbCrLf}{vbCrLf}另外，你还需要同意 PCL CE 大厅相关隐私政策及《Natayark OpenID 服务条款》。", "联机大厅协议授权",
                                                     "我已阅读并同意", "拒绝并返回", "查看相关隐私协议",
                                                     Button3Action:=Sub() OpenWebsite("https://www.pclc.cc/privacy/personal-info-brief.html"))
                                    Case 1
@@ -35,12 +37,10 @@
                                End Select
                            End If
                        End Sub)
-        If IsLoad Then Exit Sub
-        IsLoad = True
         IsMcWatcherRunning = True
         GetAnnouncement()
         If Not String.IsNullOrWhiteSpace(Setup.Get("LinkNaidRefreshToken")) Then
-            If Not String.IsNullOrWhiteSpace(Setup.Get("LinkNaidRefreshExpiresAt")) AndAlso Convert.ToDateTime(Setup.Get("LinkNaidRefreshExpiresAt")).CompareTo(DateTime.Now) > 0 Then
+            If Not String.IsNullOrWhiteSpace(Setup.Get("LinkNaidRefreshExpiresAt")) AndAlso Convert.ToDateTime(Setup.Get("LinkNaidRefreshExpiresAt")).CompareTo(DateTime.Now) < 0 Then
                 Setup.Set("LinkNaidRefreshToken", "")
                 Hint("Natayark ID 令牌已过期，请重新登录", HintType.Critical)
             Else
