bde31f3d76f7fe6d7a8dbdf8a4b0cc38772b9c9a
tangge233 <tangge233@qq.com>
2025-08-07 16:00:28 +0800

    fix(Link): 输出重定向导致的 ET 在部分情况下卡主

--- Plain Craft Launcher 2/Modules/ModLink.vb
+++ Plain Craft Launcher 2/Modules/ModLink.vb
@@ -337,10 +337,7 @@ Public Module ModLink
                 .StartInfo = New ProcessStartInfo With {
                     .FileName = $"{ETPath}\easytier-core.exe",
                     .WorkingDirectory = ETPath,
-                    .UseShellExecute = False,
-                    .CreateNoWindow = True,
-                    .RedirectStandardOutput = True,
-                    .RedirectStandardError = True
+                    .WindowStyle = ProcessWindowStyle.Hidden
                 }
             }
             '兜底
