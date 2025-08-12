1722537a3f6e1f5a44b95bed54faa1722c1e5ac3
薄奚梦灵 <boximengling@outlook.com>
2025-08-12 05:13:28 +0000

    chore(instance-manage): Update Branch & Update Ref

--- Plain Craft Launcher 2/Pages/PageInstance/PageInstanceOverall.xaml.vb
+++ Plain Craft Launcher 2/Pages/PageInstance/PageInstanceOverall.xaml.vb
@@ -1,5 +1,6 @@
-﻿Imports PCL.Core.ProgramSetup
+Imports PCL.Core.ProgramSetup
 Imports NEWSetup = PCL.Core.ProgramSetup.Setup
+Imports PCL.Core.Utils
 
 Public Class PageInstanceOverall
 
@@ -396,6 +397,25 @@ Public Class PageInstanceOverall
         Catch ex As Exception
             Log(ex, "删除实例 " & PageInstanceLeft.Instance.Name & " 失败", LogLevel.Msgbox)
         End Try
+    End Sub
+
+    '删除实例
+    '修改此代码时，同时修改 PageSelectRight 中的代码
+    Private Sub BtnManagePatch_Click(sender As Object, e As EventArgs) Handles BtnManagePatch.Click
+        Select Case MyMsgBox($"你确定要修改 {PageInstanceLeft.Instance.Name} 吗？ {vbCrLf}修改游戏核心可能导致游戏崩溃等问题。{vbCrLf}在修改核心后，请在高级启动选项里勾选关闭文件校验选项。", Title:="修补提示", Button2:="取消")
+            Case 1
+                Dim UserInput As String = SelectFile("压缩文件(*.jar;*.zip)|*.jar;*.zip", "选择用于修补核心的文件")
+                If UserInput Is Nothing Or String.IsNullOrWhiteSpace(UserInput) Then Return
+                Hint("正在修补游戏核心，这可能需要一段时间")
+                RunInNewThread(
+                    Sub()
+                        Dim Core As New GameCore(PageInstanceLeft.Instance.Path & PageInstanceLeft.Instance.Name & ".jar")
+                        Core.AddToCore(UserInput)
+                        Hint("修补游戏核心成功", HintType.Finish)
+                    End Sub)
+            Case 2
+                Return
+        End Select
     End Sub
 
 #End Region
