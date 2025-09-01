c90f74d374a911236fffb17f2770eb14317eac8b
allMagic <116283787+allMagicNB@users.noreply.github.com>
2025-09-01 23:14:38 +0800

    fix: fix

--- Plain Craft Launcher 2/Modules/Minecraft/ModLaunch.vb
+++ Plain Craft Launcher 2/Modules/Minecraft/ModLaunch.vb
@@ -1,7 +1,7 @@
 
 Imports System.IO.Compression
 Imports System.Text.Json.Nodes
-Imports System.Net
+Imports System.Web
 Imports PCL.Core.Minecraft
 Imports PCL.Core.Utils
 Imports PCL.Core.Utils.OS
