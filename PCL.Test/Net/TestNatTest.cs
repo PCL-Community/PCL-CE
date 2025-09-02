85e5643d7fcc5d604aa8296d9ac98b4a1c4d12c7
tangge233 <tangge233@qq.com>
2025-09-02 14:38:26 +0800

    feat: 完成部分 Stun Client

---
+++ PCL.Test/Net/TestNatTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PCL.Core.Net;
+using PCL.Core.Net.Nat.Stun;
+
+namespace PCL.Test.Net;
+
+[TestClass]
+public class TestNatTest
+{
+    [TestMethod]
+    public async Task Test1()
+    {
+        var instance = new StunClient();
+        var response = await StunRequestBuilder
+            .Create(instance)
+            .WithMessageType(StunMessageType.BindingRequest)
+            .GetResponseAsync(TestContext.CancellationTokenSource.Token);
+        Assert.IsNotNull(response);
+        Console.WriteLine(response.ToString());
+        var test = new NatTest(instance);
+        Console.WriteLine((await test.GetPublicEndPointAsync(TestContext.CancellationTokenSource.Token))?.ToString());
+    }
+
+    public TestContext TestContext { get; set; }
+}
