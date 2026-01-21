fd558377d6f45435687963dba7d78fbe7e6c28e6
吴桐 <162002839+wtommy932@users.noreply.github.com>
2026-01-21 13:47:08 +0800

    refactor: Resolve conversation

--- Plain Craft Launcher 2/FormMain.xaml.vb
+++ Plain Craft Launcher 2/FormMain.xaml.vb
@@ -460,7 +460,8 @@ Public Class FormMain
     ''' 正常关闭程序。程序将在执行此方法后约 0.3s 退出。
     ''' </summary>
     ''' <param name="SendWarning">是否在还有下载任务未完成时发出警告。</param>
-    Public Async Sub EndProgram(SendWarning As Boolean, Optional hasUpdated As Boolean = False)
+    ''' <param name="isUpdating">是否正在更新重启</param>
+    Public Async Sub EndProgram(SendWarning As Boolean, Optional isUpdating As Boolean = False)
         '发出警告
         If SendWarning AndAlso HasDownloadingTask() Then
             If MyMsgBox("还有下载任务尚未完成，是否确定退出？", "提示", "确定", "取消") = 1 Then
@@ -510,10 +511,10 @@ Public Class FormMain
                         Visibility = Visibility.Collapsed
                         ShowInTaskbar = False
                     End Sub, 210),
-                    AaCode(Sub() EndProgramForce(force:=False, hasUpdated:=hasUpdated), 230)
+                    AaCode(Sub() EndProgramForce(force:=False, isUpdating:=isUpdating), 230)
                 }, "Form Close")
             Else
-                EndProgramForce(force:=False, hasUpdated:=hasUpdated)
+                EndProgramForce(force:=False, isUpdating:=isUpdating)
             End If
             Log("[System] 收到关闭指令")
         End Sub)
@@ -522,13 +523,13 @@ Public Class FormMain
     Public Shared Async Sub EndProgramForce(
                                             Optional ReturnCode As ProcessReturnValues = ProcessReturnValues.Success, 
                                             Optional force As Boolean = True,
-                                            Optional hasUpdated As Boolean = False)
+                                            Optional isUpdating As Boolean = False)
         'On Error Resume Next
         '关闭联机大厅
         'Await LobbyController.CloseAsync().ConfigureAwait(False)
         IsProgramEnded = True
         AniControlEnabled += 1
-        If IsUpdateWaitingRestart AndAlso Not hasUpdated Then UpdateRestart(False, triggerRestart := False)
+        If IsUpdateWaitingRestart AndAlso Not isUpdating Then UpdateRestart(False, triggerRestart := False)
         If ReturnCode = ProcessReturnValues.Exception Then
             If Not IsLogShown Then
                 FeedbackInfo()
