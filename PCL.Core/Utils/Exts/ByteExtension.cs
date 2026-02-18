571ce7609b004f4a4cbcf30b28a0ba44caf25248
薄奚梦灵 <boximengling@outlook.com>
2026-02-18 19:09:39 +0800

    春时节 天破颜

--- PCL.Core/Utils/Exts/ByteExtension.cs
+++ PCL.Core/Utils/Exts/ByteExtension.cs
@@ -17,5 +17,12 @@ public static class ByteExtension
 
             return sb.ToString();
         }
+        
+    }
+    extension(byte[] input)
+    {
+        public string FromByteToB64() => Convert.ToBase64String(input);
+        public string FromBytesToB64UrlSafe() => input.FromByteToB64().FromB64ToB64UrlSafe();
+        
     }
 }
