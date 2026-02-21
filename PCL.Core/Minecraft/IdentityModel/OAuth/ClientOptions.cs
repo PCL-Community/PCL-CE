e46c27d549463f1105580f19dcdb545d8aff12b6
薄奚梦灵 <boximengling@outlook.com>
2026-02-21 14:10:00 +0800

    肩上的风 和手心的力 会化作你掌纹的痕迹 每个日出拥抱崭新的你

---
+++ PCL.Core/Minecraft/IdentityModel/OAuth/ClientOptions.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace PCL.Core.Minecraft.IdentityModel.OAuth;
+
+public record OAuthClientOptions
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public Dictionary<string,string>? Headers { get; set; }
+    public required EndpointMeta Meta { get; set; }
+    public required Func<HttpClient> GetClient { get; set; }
+
+    public required string RedirectUri { get; set; }
+    public required string ClientId { get; set; }
+}
