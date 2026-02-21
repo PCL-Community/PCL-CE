3e0ab3f1e6424f56c0094a1433c703571dd7c520
薄奚梦灵 <boximengling@outlook.com>
2026-02-21 14:08:20 +0800

    此刻的你 找到你 我看见大雾散去 感谢你无数次拼命地拉住了自己

---
+++ PCL.Core/Minecraft/IdentityModel/OAuth/EndpointMeta.cs
@@ -0,0 +1,8 @@
+namespace PCL.Core.Minecraft.IdentityModel.OAuth;
+
+public record EndpointMeta
+{
+    public string? DeviceEndpoint { get; set; }
+    public required string AuthorizeEndpoint { get; set; }
+    public required string TokenEndpoint { get; set; }
+}
