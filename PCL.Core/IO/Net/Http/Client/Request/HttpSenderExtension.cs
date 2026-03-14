b9b66157530814cc75265595c6446073ee416633
tangge233 <tangge233@qq.com>
2026-03-14 22:58:19 +0800

    chore: add log

--- PCL.Core/IO/Net/Http/Client/Request/HttpSenderExtension.cs
+++ PCL.Core/IO/Net/Http/Client/Request/HttpSenderExtension.cs
@@ -1,5 +1,7 @@
 using PCL.Core.App;
+using PCL.Core.Logging;
 using PCL.Core.Utils.Exts;
+using System;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
@@ -8,30 +10,60 @@ namespace PCL.Core.IO.Net.Http.Client.Request;
 
 public static class HttpSenderExtension
 {
-    extension (HttpRequestMessage requestMessage)
+    extension(HttpRequestMessage requestMessage)
     {
-        public async Task<HttpResponseMessage> SendAsync(HttpClient? httpClient = null, bool withLauncherMetadata = true, HttpCompletionOption httpCompletionOption = HttpCompletionOption.ResponseContentRead, int retryTimes = 3, CancellationToken cancellationToken = default)
+        public async Task<HttpResponseMessage> SendAsync(
+            HttpClient? httpClient = null,
+            bool addMetedata = true,
+            bool enableLogging = true,
+            HttpCompletionOption httpCompletionOption = HttpCompletionOption.ResponseContentRead,
+            int retryTimes = 3,
+            CancellationToken cancellationToken = default)
         {
             using var request = requestMessage;
             httpClient ??= NetworkService.GetClient();
 
-            if (withLauncherMetadata)
+            if(addMetedata)
             {
-                requestMessage.Headers.TryAddWithoutValidation("User-Agent", $"PCL-Community/PCL2-CE/{Basics.VersionName} (pclc.cc)");
-                requestMessage.Headers.TryAddWithoutValidation("Referer", $"https://{Basics.VersionCode}.ce.open.pcl2.server/");
+                request
+                    .WithHeader("User-Agent", $"PCL-Community/PCL2-CE/{Basics.VersionName} (pclc.cc)")
+                    .WithHeader("Referer", $"https://{Basics.VersionCode}.ce.open.pcl2.server/");
             }
 
-            return await NetworkService.GetRetryPolicy(retryTimes)
-                .ExecuteAsync(async token =>
-                {
-                    using var requestCopy = await request
-                        .CloneAsync()
-                        .ConfigureAwait(false);
-                    return await httpClient
-                        .SendAsync(requestCopy, httpCompletionOption, token)
-                        .ConfigureAwait(false);
-                }, cancellationToken)
+            var requestId = Guid.NewGuid().ToString();
+            if (enableLogging)
+                LogWrapper.Info(
+                    "Request",
+                    $"Send request to {request.RequestUri} (method = {request.Method}, id = {requestId})");
+
+            var resp = await NetworkService.GetRetryPolicy(retryTimes)
+                .ExecuteAsync(
+                    async token =>
+                    {
+                        if (enableLogging)
+                            LogWrapper.Debug("Request", $"Try attempt (id = {requestId})");
+                        try
+                        {
+                            using var requestCopy = await request
+                                .CloneAsync()
+                                .ConfigureAwait(false);
+                            return await httpClient
+                                .SendAsync(requestCopy, httpCompletionOption, token)
+                                .ConfigureAwait(false);
+                        } 
+                        catch(Exception ex)
+                        {
+                            LogWrapper.Error(ex, "Request", $"Try attempt failed (id = {requestId})");
+                            throw;
+                        }
+                    },
+                    cancellationToken,
+                    false)
                 .ConfigureAwait(false);
+
+            if (enableLogging)
+                LogWrapper.Info("Request", $"End request, got http status code {resp.StatusCode} (id = {requestId})");
+            return resp;
         }
     }
 }
