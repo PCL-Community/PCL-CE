4aec81bf77e5757e972660a5ee7961e65a76061f
xrlzu <xrlzu@outlook.com>
2026-03-31 20:09:46 +0800

    移除莫名其妙的空格

--- PCL.Core/Minecraft/Saves/Models/LevelDataInfo.cs
+++ PCL.Core/Minecraft/Saves/Models/LevelDataInfo.cs
@@ -7,11 +7,11 @@ namespace PCL.Core.Minecraft.Saves.Models;
 /// </summary>
 public class LevelDataInfo
 {
-    public string LevelName { get; set; } = null!;   
-    public string Seed { get; set; } = null!;    
+    public string LevelName { get; set; } = null!;
+    public string Seed { get; set; } = null!;
     public string SpawnPoint { get; set; } = null!;
     public string GameType { get; set; } = null!;
-    public string DifficultyDisplay { get; set; } = null!;       
+    public string DifficultyDisplay { get; set; } = null!;
     public DateTime LastPlayed { get; set; }
     public TimeSpan PlayTime { get; set; }
     public string? VersionName { get; set; }
