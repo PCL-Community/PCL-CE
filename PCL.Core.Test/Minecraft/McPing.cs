5b85edeafd943d34da81541e2c650f522ba9fc71
tangge233 <tangge233@qq.com>
2026-04-06 13:50:24 +0800

    fix: ip incorrect

--- PCL.Core.Test/Minecraft/McPing.cs
+++ PCL.Core.Test/Minecraft/McPing.cs
@@ -2,7 +2,6 @@ using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Threading.Tasks;
 using System.Net.Sockets;
-using PCL.Core.Link;
 using PCL.Core.Link.McPing;
 
 namespace PCL.Core.Test.Minecraft;
@@ -15,13 +14,15 @@ public class McPingTest
     {
         using var so = new Socket(SocketType.Stream, ProtocolType.Tcp);
         using var ping2 = McPingServiceFactory.CreateService("mc.hypixel.net", 25565);
-        var res = await ping2.PingAsync();
+        var res = await ping2.PingAsync(TestContext.CancellationToken).ConfigureAwait(false);
         Assert.IsNotNull(res);
         Console.WriteLine(res.Description);
 
-        using var ping1 = McPingServiceFactory.CreateService("mc.233.cn", 25565);
-        res = await ping1.PingAsync();
+        using var ping1 = McPingServiceFactory.CreateService("mc233.cn", 25565);
+        res = await ping1.PingAsync(TestContext.CancellationToken).ConfigureAwait(false);
         Assert.IsNotNull(res);
         Console.WriteLine(res.Description);
     }
+
+    public TestContext TestContext { get; set; }
 }
