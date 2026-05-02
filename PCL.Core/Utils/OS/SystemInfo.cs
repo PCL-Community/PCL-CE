1ca856f204fbe3896fb1ca2106b2bccd7dfd43ae
LuLu-ling <LuLu-ling@outlook.com>
2026-05-02 14:20:09 +0800

    refactor: Update SystemInfo class to improve readability and structure

--- PCL.Core/Utils/OS/SystemInfo.cs
+++ PCL.Core/Utils/OS/SystemInfo.cs
@@ -6,47 +6,46 @@ using PCL.Core.Logging;
 
 namespace PCL.Core.Utils.OS;
 
-public class SystemInfo
+public static class SystemInfo
 {
     public static string CPUName = null!;
 
     /// <summary>
-    ///     系统 GPU 信息
+    /// 系统 GPU 信息
     /// </summary>
     public static List<GPUInfo> GPUs = new();
 
     /// <summary>
-    ///     已安装物理内存大小，单位 MB
+    /// 已安装物理内存大小，单位 MB
     /// </summary>
     public static long SystemMemorySize = (long)KernelInterop.GetPhysicalMemoryBytes().Total / 1024 / 1024;
 
     /// <summary>
-    ///     系统信息描述，例如 Microsoft Windows 11 专业工作站版 10.0.22635.0
+    /// 系统信息描述，例如 Microsoft Windows 11 专业工作站版 10.0.22635.0
     /// </summary>
     public static string OSInfo = RuntimeInformation.OSDescription + " " + Environment.OSVersion.Version;
 
     public class GPUInfo
     {
+        public string Name = null!;
+      
         public string DriverVersion = null!;
 
         /// <summary>
-        ///     显存大小，单位 MB
+        /// 显存大小，单位 MB
         /// </summary>
         public long Memory;
-
-        public string Name = null!;
     }
 
     /// <summary>
-    ///     获取系统信息，例如 CPU 与 GPU，并存储到 CPUName 和 GPUs
+    /// 获取系统信息，例如 CPU 与 GPU，并存储到 CPUName 和 GPUs
     /// </summary>
     public static void GetSystemInfo()
     {
         // CPU
         try
         {
-            var searcher = new ManagementObjectSearcher(@"root\CIMV2", "SELECT * FROM Win32_Processor");
-
+            using var searcher = new ManagementObjectSearcher(@"root\CIMV2", "SELECT * FROM Win32_Processor");
             foreach (ManagementObject queryObj in searcher.Get())
             {
                 CPUName = queryObj["Name"].ToString().Trim();
@@ -61,19 +60,15 @@ public class SystemInfo
         // GPU
         try
         {
-            var searcher = new ManagementObjectSearcher(@"root\CIMV2", "SELECT * FROM Win32_VideoController");
-
+            using var searcher = new ManagementObjectSearcher(@"root\CIMV2", "SELECT * FROM Win32_VideoController");
             foreach (ManagementObject queryObj in searcher.Get())
             {
                 var gpuInfo = new GPUInfo();
 
-                if (queryObj["Name"] is not null) gpuInfo.Name = queryObj["Name"].ToString();
-                if (queryObj["AdapterRAM"] is not null)
-                {
-                    var ramMB = Convert.ToInt64(queryObj["AdapterRAM"]) / (1024 * 1024);
-                    gpuInfo.Memory = ramMB;
-                }
-
+                if (queryObj["Name"] is not null)
+                    gpuInfo.Name = queryObj["Name"].ToString();
+                if (queryObj["AdapterRAM"] is not null and not DBNull)
+                    gpuInfo.Memory = Convert.ToInt64(queryObj["AdapterRAM"]) / (1024 * 1024);
                 if (queryObj["DriverVersion"] is not null)
                     gpuInfo.DriverVersion = queryObj["DriverVersion"].ToString();
 
