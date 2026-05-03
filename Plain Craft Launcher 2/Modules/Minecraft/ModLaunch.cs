a4f1797daa91705ecd2dabf50ca7a4b6390b24cd
林轻语 <boximengling@outlook.com>
2026-05-04 04:39:54 +0800

    chore: trim space

--- Plain Craft Launcher 2/Modules/Minecraft/ModLaunch.cs
+++ Plain Craft Launcher 2/Modules/Minecraft/ModLaunch.cs
@@ -1798,7 +1798,7 @@ public static class ModLaunch
         {
             using var responseStream = ex.Response?.Content.ReadAsStream();
             if (responseStream is null) return false;
-            var result  = JsonSerializer.Deserialize<YggdrasilAuthenticateResult>(responseStream);
+            var result = JsonSerializer.Deserialize<YggdrasilAuthenticateResult>(responseStream);
             if (result?.ErrorMessage is null) return false;
             message = result.ErrorMessage;
             return true;
