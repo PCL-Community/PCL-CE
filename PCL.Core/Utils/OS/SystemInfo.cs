49aeb2747f6de43c393f492a90a5bf860371d3b8
LuLu-ling <LuLu-ling@outlook.com>
2026-05-26 20:46:08 +0800

    1

--- PCL.Core/Utils/OS/SystemInfo.cs
+++ PCL.Core/Utils/OS/SystemInfo.cs
@@ -9,12 +9,12 @@ public static class SystemInfo
     /// <summary>
     /// 是否为 32 位系统。
     /// </summary>
-    public static bool Is32BitSystem => !Environment.Is64BitOperatingSystem;
+    public static bool Is32BitSystem = !Environment.Is64BitOperatingSystem;
 
     /// <summary>
     /// 是否为 ARM64 架构。
     /// </summary>
-    public static bool IsArm64System => RuntimeInformation.OSArchitecture == Architecture.Arm64;
+    public static bool IsArm64System = RuntimeInformation.OSArchitecture == Architecture.Arm64;
 
     /// <summary>
     /// 是否使用 GBK 编码。
