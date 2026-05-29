9696a497c633fd92e4ed2dcd1e140922b776fc05
WhiteCAT <64885812+whitecat346@users.noreply.github.com>
2026-05-29 18:40:26 +0800

    fix: fix ref error; add HashStorage again

--- PCL.Core/IO/Net/NetworkService.cs
+++ PCL.Core/IO/Net/NetworkService.cs
@@ -1,14 +1,14 @@
+using Microsoft.Extensions.DependencyInjection;
+using PCL.Core.App;
+using PCL.Core.App.IoC;
+using PCL.Core.IO.Net.Http;
+using PCL.Core.IO.Net.Http.Cache;
+using PCL.Core.Logging;
+using Polly;
 using System;
 using System.IO;
 using System.Net;
 using System.Net.Http;
-using Microsoft.Extensions.DependencyInjection;
-using PCL.Core.App;
-using PCL.Core.App.IoC;
-using PCL.Core.IO.Net.Http.Cache;
-using PCL.Core.IO.Net.Http.Client;
-using PCL.Core.Logging;
-using Polly;
 
 namespace PCL.Core.IO.Net;
 
@@ -16,14 +16,15 @@ namespace PCL.Core.IO.Net;
 [LifecycleScope("network", "网络服务")]
 public partial class NetworkService
 {
-    private static HttpCacheRepository _repo = new(Path.Combine(Paths.Temp,"cache","cache.db"),Path.Combine(Paths.Temp,"cache"));
-    
+    private static HttpCacheRepository _repo = new(Path.Combine(Paths.Temp, "cache", "cache.db"), Path.Combine(Paths.Temp, "cache"));
+
     private static ServiceProvider? _provider;
     private static IHttpClientFactory? _factory;
 
     [LifecycleStart]
     private static void _Start()
-    {   _repo.Initialize();
+    {
+        _repo.Initialize();
         var services = new ServiceCollection();
         services.AddHttpClient("default")
             .ConfigurePrimaryHttpMessageHandler(() => new SocketsHttpHandler
@@ -42,25 +43,26 @@ public partial class NetworkService
         services.AddHttpClient("cache").ConfigurePrimaryHttpMessageHandler(() => new HttpCacheHandler(
             new SocketsHttpHandler
             {
-            UseProxy = true,
-            UseCookies = false,
-            AutomaticDecompression = DecompressionMethods.All,
-            Proxy = HttpProxyManager.Instance,
-            AllowAutoRedirect = true,
-            MaxAutomaticRedirections = 20,
-            ConnectCallback = Config.Network.EnableDoH
+                UseProxy = true,
+                UseCookies = false,
+                AutomaticDecompression = DecompressionMethods.All,
+                Proxy = HttpProxyManager.Instance,
+                AllowAutoRedirect = true,
+                MaxAutomaticRedirections = 20,
+                ConnectCallback = Config.Network.EnableDoH
                 ? HostConnectionHandler.Instance.GetConnectionAsync
                 : null
-        },_repo));
-        
+            }, _repo));
+
 
         _provider = services.BuildServiceProvider();
         _factory = _provider.GetRequiredService<IHttpClientFactory>();
-        
+
     }
 
     [LifecycleStop]
-    private static void _Stop() {
+    private static void _Stop()
+    {
         _provider?.Dispose();
     }
 
@@ -90,7 +92,7 @@ public partial class NetworkService
     /// <param name="retry">最大重试次数</param>
     /// <param name="retryPolicy">定义重试器行为</param>
     /// <returns>AsyncPolicy</returns>
-    public static AsyncPolicy GetRetryPolicy(int retry = 3, Func<int,TimeSpan>? retryPolicy = null)
+    public static AsyncPolicy GetRetryPolicy(int retry = 3, Func<int, TimeSpan>? retryPolicy = null)
     {
         retryPolicy ??= _DefaultSleepDurationProvider;
 
