cdd10f7e10baa3d10ead7379af3a7e735fe19cd9
ClovenBugle <yan_hun233@outlook.com>
2026-06-05 21:43:19 +0800

    踹飞 BOM 老母二里地

--- PCL.Core/UI/Controls/SvgIcon/SvgIconElement.cs
+++ PCL.Core/UI/Controls/SvgIcon/SvgIconElement.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Windows.Media;
 
 namespace PCL.Core.UI.Controls.SvgIcon;
--- Plain Craft Launcher 2/Pages/PageDownload/Comp/PageDownloadCompDetail.xaml.cs
+++ Plain Craft Launcher 2/Pages/PageDownload/Comp/PageDownloadCompDetail.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.ObjectModel;
 using System.Diagnostics.Eventing.Reader;
 using System.IO;
