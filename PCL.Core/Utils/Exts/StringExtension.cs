e7db2b31f2234e43edc22be1ea7b06daaf5165a3
SaltWood_233 <105980161+SALTWOOD@users.noreply.github.com>
2026-06-20 03:19:50 +0800

    Update PCL.Core/Utils/Exts/StringExtension.cs

--- PCL.Core/Utils/Exts/StringExtension.cs
+++ PCL.Core/Utils/Exts/StringExtension.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.Numerics;
 using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Text;
 using System.Text.RegularExpressions;
 
 namespace PCL.Core.Utils.Exts;
