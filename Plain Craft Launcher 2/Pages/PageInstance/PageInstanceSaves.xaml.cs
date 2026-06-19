e53c416fc4051e20d8bff18a0ab3bbfaa28f7214
SaltWood_233 <105980161+SALTWOOD@users.noreply.github.com>
2026-06-19 20:21:58 +0800

    feat(save): add download resource button

--- Plain Craft Launcher 2/Pages/PageInstance/PageInstanceSaves.xaml.cs
+++ Plain Craft Launcher 2/Pages/PageInstance/PageInstanceSaves.xaml.cs
@@ -410,6 +410,11 @@ public partial class PageInstanceSaves : IRefreshable
         ModMain.frmMain.BtnExtraDownload.Ribble();
     }
 
+    private void BtnDownloadNew_Click(object sender, MouseButtonEventArgs e)
+    {
+        ModMain.frmMain.PageChange(FormMain.PageType.Download, FormMain.PageSubType.DownloadWorld);
+    }
+
     #region 搜索和排序
 
     private SortMethod _currentSortMethod = SortMethod.FileName;
